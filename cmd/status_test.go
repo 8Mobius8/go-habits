5e4be7a01d9feb10e253e28ea603351515c0707b
Mark Odell <m4rk.odell@gmail.com>
2018-09-05 18:22:55 -0700

    Makefile! :tada: Remove lisencing info from source. isup is now status. Wait for habitica use cURL.

--- cmd/status_test.go
+++ cmd/status_test.go
@@ -7,18 +7,18 @@ import (
 	. "github.com/onsi/gomega"
 )
 
-var _ = Describe("Isup", func() {
+var _ = Describe("Status", func() {
 	It("returns the funnies when Habitica is reachable.", func() {
 		var resp api.Status
 		resp.Status = "up"
 
-		Expect(IsUpMessage(resp)).To(Equal("Habitica is reachable, GO catch all those pets!"))
+		Expect(StatusMessage(resp)).To(Equal("Habitica is reachable, GO catch all those pets!"))
 	})
 
 	It("returns the sad when Habitica is unreachable.", func() {
 		var resp api.Status
 		resp.Status = "down"
 
-		Expect(IsUpMessage(resp)).To(Equal(":( Habitica is unreachable."))
+		Expect(StatusMessage(resp)).To(Equal(":( Habitica is unreachable."))
 	})
 })
--- integration/wait-for-habitica-api.sh
+++ integration/wait-for-habitica-api.sh
@@ -1,17 +1,16 @@
 #!/bin/bash
-
 set -e
 
 cmd="$@"
 date_to_wait_until=$(date -d "+1mins" +%s)
-echo "Waiting Habitica API to be up"
-until go-habits isup > /dev/null 2>&1; do
+echo "Waiting Habitica API to be up @ $SERVER"
+until curl -s $SERVER > /dev/null 2>&1; do
   >&2 printf "."
   sleep 1
   if [ $date_to_wait_until  -lt $(date +%s) ]; then
     echo
     echo "Habitica API took too long to start"
-    exit 0
+    exit 1
   fi
 done
 
