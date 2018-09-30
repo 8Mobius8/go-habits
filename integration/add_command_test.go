f84a71054b62c32c0bef5a7815c3e753e4f51ecc
Mark Odell <m4rk.odell@gmail.com>
2018-09-29 18:36:41 -0700

    `go-habits add` command :tada:
    Top to bottom add command.
    Refactored PrintTodos to use a new function PrintTodo.
    Simplified config.yml file for integration tests.

---
+++ integration/add_command_test.go
@@ -0,0 +1,44 @@
+package integration
+
+import (
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+	"github.com/onsi/gomega/gbytes"
+	"github.com/onsi/gomega/gexec"
+)
+
+var _ = Describe("go-habits add command", func() {
+	It("actually exists", func() {
+		session := GoHabits("add", "-h")
+		Eventually(session).Should(gexec.Exit(0))
+	})
+
+	Context("Given the user is already signed-in", func() {
+		BeforeEach(func() {
+			TouchConfigFile()
+			expectSuccessfulLogin(userName, password)
+		})
+		AfterEach(func() {
+			ResetUser(HABITICA_API, apiID, apiToken)
+			RemoveConfigFile()
+		})
+
+		Describe("Creating a new todo", func() {
+			It("will print newly created task as confirmation", func() {
+				s := GoHabits("add", "clean my fishbowl")
+				Eventually(s).Should(gbytes.Say("clean my fishbowl"))
+				Eventually(s).Should(gexec.Exit(0))
+			})
+
+			It("will print newly created task and order as confirmation", func() {
+				s := GoHabits("add", "clean my fishbowl")
+				Eventually(s).Should(gexec.Exit(0))
+
+				s = GoHabits("add", "make bed")
+				Eventually(s).Should(gbytes.Say("2"))
+				Eventually(s).Should(gbytes.Say("make bed"))
+				Eventually(s).Should(gexec.Exit(0))
+			})
+		})
+	})
+})
