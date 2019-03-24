d3d0c8217be213197a11ac968e1e6a40ff4fdd33
Mark Odell <m4rk.odell@gmail.com>
2019-03-23 21:15:50 -0700

    Tests add using files

--- integration/commands/add_command_test.go
+++ integration/commands/add_command_test.go
@@ -1,7 +1,9 @@
 package commands
 
 import (
+	"fmt"
 	"io/ioutil"
+	"math/rand"
 	"os"
 
 	. "github.com/8Mobius8/go-habits/integration"
@@ -52,29 +54,72 @@ var _ = Describe("go-habits add command", func() {
 		})
 
 		Describe("Creating a new todo using file", func() {
-			tasksFile := "tasks.txt"
+			var tasksFileName string
 			BeforeEach(func() {
-				AddTaskToFile(tasksFile, "A new task")
+				f, err := ioutil.TempFile("./", "tasks")
+				f.Close()
+				Expect(err).ToNot(HaveOccurred())
+				tasksFileName = f.Name()
 			})
 			AfterEach(func() {
-				RemoveTaskFile(tasksFile)
+				RemoveFile(tasksFileName)
+			})
+
+			It("will accept --file or -f to specify file", func() {
+				s := GoHabits("add", "-f", tasksFileName)
+				Eventually(s).Should(gexec.Exit(0))
+
+				s = GoHabits("add", "--file", tasksFileName)
+				Eventually(s).Should(gexec.Exit(0))
 			})
 
 			It("will print new created task as confirmation", func() {
-				s := GoHabits("add", "-f", "tasks.txt", "--log", "debug")
+				AppendToFile(tasksFileName, TaskLine("A new task"))
+				AppendToFile(tasksFileName, TaskLine("A new task #dup"))
+
+				s := GoHabits("add", "-f", tasksFileName)
 				Eventually(s).Should(gbytes.Say("A new task"))
+				Eventually(s).Should(gbytes.Say("A new task #dup"))
+				Eventually(s).Should(gexec.Exit(0))
+			})
+
+			It("will ignore lines that do not have prefixed task list", func() {
+				AppendToFile(tasksFileName, TaskLine("A new task"))
+				for i := 1; i < rand.Intn(5); i++ {
+					AppendToFile(tasksFileName, "a line that isn't a task\n")
+				}
+
+				s := GoHabits("add", "-f", tasksFileName)
+				Eventually(s).Should(gbytes.Say("A new task"))
+				Eventually(s).Should(gexec.Exit(0))
+			})
+
+			It("will print 'missing' lines that have an empty task title", func() {
+				AppendToFile(tasksFileName, TaskLine(""))
+
+				s := GoHabits("add", "-f", tasksFileName)
+				Eventually(s).Should(gbytes.Say("missing text"))
 				Eventually(s).Should(gexec.Exit(0))
 			})
 		})
 	})
 })
 
-func AddTaskToFile(filePath, taskTitle string) {
-	err := ioutil.WriteFile(filePath, []byte("[ ] "+taskTitle+"\n"), 0644)
-	Expect(err).ShouldNot(HaveOccurred())
+func AppendToFile(filepath, s string) {
+	f, err := os.OpenFile(filepath, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0600)
+	Expect(err).ToNot(HaveOccurred())
+
+	defer f.Close()
+
+	_, err = f.WriteString(s)
+	Expect(err).ToNot(HaveOccurred())
 }
 
-func RemoveTaskFile(filePath string) {
+func TaskLine(title string) string {
+	return fmt.Sprintf("[ ] %s\n", title)
+}
+
+func RemoveFile(filePath string) {
 	err := os.Remove(filePath)
 	if err != nil {
 		if os.IsNotExist(err) {
